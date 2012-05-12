d7f9b76bf4e20e777f4313cc8155130452770de6
FUKUI Osamu <ofukui@irubysystems.com>
2012-05-13 00:58:17 +0900

    tatekoto start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tatekoto::Application.config.secret_token = 'ed4ce8c48628a9a895140916ddcef7978a660c200177cda62d319d8f9741b6b734a9314a2536a6eda5d4bfb9b739c2aa97ae2cacd3cd28f02b4913231a08d8a2'
